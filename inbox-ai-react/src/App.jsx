21fb924199195c15651e5cf8b124eedd4ead7eac
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-09-20 11:32:05 +0530

    const variable with hook and use state created

--- inbox-ai-react/src/App.jsx
+++ inbox-ai-react/src/App.jsx
@@ -1,11 +1,13 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [emailContenet, setEmailContent] = useState('');
+   const [tone, setTone] = useState('');
+    const [generatedReply, setGeneratedReply] = useState('');
+     const [loading, setLoading] = useState('');
+      const [error, setError] = useState('');
   return (
     <>
       
